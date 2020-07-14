6c8c9a315a2f8c55b9c18a41c5fb2f65aad0c8e1
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-14 16:23:06 -0400

    cleaned up controllers

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -2,7 +2,6 @@ let express = require("express");
 let router = express.Router();
 let burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     let allBurgers = {
