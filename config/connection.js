72905801f4c3fe7fc80d1af40aa8e65a99699667
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-14 17:02:58 -0400

    added process.env instead of process.send

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 let mysql = require("mysql");
 var connection;
 
+
+
 if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.send.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   connection = mysql.createConnection({
     host: "localhost",
